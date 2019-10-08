8b3a7199c28d807690e41d73e463f35ab16d408d
Chad Kinsella <ismisechad@gmail.com>
2019-10-08 23:09:09 +0100

    Set food name to lowercase for DB initialisation helper functions.

--- backend/src/resolvers/dbInitUtil.js
+++ backend/src/resolvers/dbInitUtil.js
@@ -1,105 +1,103 @@
-const { capitaliseWords } = require('../utils');
-
 function createFood(food, foodTyoe, ctx, info) {
-    const newFood = {
-        name: capitaliseWords(food),
-        types: foodTyoe,
-      }
+  const newFood = {
+    name: food.toLowerCase(),
+    types: foodTyoe,
+  };
 
-    return ctx.db.mutation.createFood({
+  return ctx.db.mutation.createFood({
     data: {
-        ...newFood,
-        types: {
-            set: foodTyoe
-        }
+      ...newFood,
+      types: {
+        set: foodTyoe
+      }
     }
-    }, info);
+  }, info);
 }
 
 function initialiseBreadsAndPastries(ctx, info) {
-    const breadData = require('../foods/breads_and_pastries.json');
+  const breadData = require('../foods/breads_and_pastries.json');
 
-    const breads =  breadData.breads.map((bread) => {
-      return createFood(bread, ['BREAD', 'BAKED'], ctx, info);
-    });
+  const breads = breadData.breads.map((bread) => {
+    return createFood(bread, ['BREAD', 'BAKED'], ctx, info);
+  });
 
-    const pastries =  breadData.pastries.map((pastry) => {
-      return createFood(pastry, ['BAKED'], ctx, info);
-    });
+  const pastries = breadData.pastries.map((pastry) => {
+    return createFood(pastry, ['BAKED'], ctx, info);
+  });
 
-    return [...breads, ...pastries];
+  return [...breads, ...pastries];
 }
 
 function initialiseDairyAndCheese(ctx, info) {
-    const dairyData = require('../foods/dairy.json');
+  const dairyData = require('../foods/dairy.json');
 
-    const dairies =  dairyData.dairy.map((dairy) => {
-      return createFood(dairy, ['DAIRY'], ctx, info);
-    });
+  const dairies = dairyData.dairy.map((dairy) => {
+    return createFood(dairy, ['DAIRY'], ctx, info);
+  });
 
-    const cheeseData = require('../foods/cheeses.json');
+  const cheeseData = require('../foods/cheeses.json');
 
-    const cheeses =  cheeseData.cheeses.map((cheese) => {
-      return createFood(cheese, ['CHEESE', 'DAIRY'], ctx, info);
-    });
+  const cheeses = cheeseData.cheeses.map((cheese) => {
+    return createFood(cheese, ['CHEESE', 'DAIRY'], ctx, info);
+  });
 
-    return [...dairies, ...cheeses];
+  return [...dairies, ...cheeses];
 }
 
 function initialiseCondiments(ctx, info) {
-    const condimentData = require('../foods/condiments.json');
+  const condimentData = require('../foods/condiments.json');
 
-    return condimentData.condiments.map((condiment) => {
-      return createFood(condiment, ['CONDIMENT'], ctx, info);
-    });
+  return condimentData.condiments.map((condiment) => {
+    return createFood(condiment, ['CONDIMENT'], ctx, info);
+  });
 }
 
 function initialiseFruits(ctx, info) {
-    const fruitData = require('../foods/fruits.json');
+  const fruitData = require('../foods/fruits.json');
 
-    return fruitData.fruits.map((fruit) => {
-      return createFood(fruit, ['FRUIT'], ctx, info);
-    });
+  return fruitData.fruits.map((fruit) => {
+    return createFood(fruit, ['FRUIT'], ctx, info);
+  });
 }
 
 function initialiseGrains(ctx, info) {
-    const grainData = require('../foods/grains.json');
+  const grainData = require('../foods/grains.json');
 
-    return grainData.grains.map((grain) => {
-      return createFood(grain, ['GRAIN'], ctx, info);
-    });
+  return grainData.grains.map((grain) => {
+    return createFood(grain, ['GRAIN'], ctx, info);
+  });
 }
 
 function initialiseMeats(ctx, info) {
-    const meatData = require('../foods/meats.json');
+  const meatData = require('../foods/meats.json');
 
-    return meatData.meats.map((meat) => {
-      return createFood(meat, ['MEAT'], ctx, info);
-    });
+  return meatData.meats.map((meat) => {
+    return createFood(meat, ['MEAT'], ctx, info);
+  });
 }
 
 function initialiseNuts(ctx, info) {
-    const nutData = require('../foods/nuts.json');
+  const nutData = require('../foods/nuts.json');
 
-    return nutData.nuts.map((nut) => {
-      return createFood(nut, ['NUT'], ctx, info);
-    });
+  return nutData.nuts.map((nut) => {
+    return createFood(nut, ['NUT'], ctx, info);
+  });
 }
 
 function initialisePastas(ctx, info) {
-    const pastaData = require('../foods/pastas.json');
+  const pastaData = require('../foods/pastas.json');
 
-    return pastaData.pastas.map((pasta) => {
-      return createFood(pasta, ['PASTA'], ctx, info);
-    });
+  return pastaData.pastas.map((pasta) => {
+    return createFood(pasta, ['PASTA'], ctx, info);
+  });
 }
 
 function initialiseVegetables(ctx, info) {
-    const vegData = require('../foods/vegetables.json');
+  const vegData = require('../foods/vegetables.json');
 
-    return vegData.vegetables.map((vegetable) => {
-      return createFood(vegetable, ['VEGETABLE'], ctx, info);
-    });
+  return vegData.vegetables.map((vegetable) => {
+    return createFood(vegetable, ['VEGETABLE'], ctx, info);
+  });
 }
 
 
